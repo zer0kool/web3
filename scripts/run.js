844b2257ba358beebf4e54f215fd014a2914f9e2
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-06 23:06:23 -0700

    init

---
+++ scripts/run.js
@@ -0,0 +1,30 @@
+const main = async () => {
+	const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+
+  console.log("Contract deployed to:", waveContract.address);
+	console.log("Contract deployed by:", owner.address);
+
+
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
