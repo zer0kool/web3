774f9e64cfd7d347075bcccbd4fc1f16bcb0b336
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-07 03:38:56 -0800

    change file to js from jsx

---
+++ app/src/components/WavesPortal/WavesPortal.js
@@ -0,0 +1,30 @@
+
+import * as React from "react";
+import { ethers } from "ethers";
+import './WavesPortal.css';
+
+export default function WavesPortal() {
+
+  const wave = () => {
+
+  }
+
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
