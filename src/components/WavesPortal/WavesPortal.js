6f7f0d0e947413a0d5dd51b78f3c9868dc2ea8e7
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-07 22:29:52 -0800

    good for staging testing

--- src/components/WavesPortal/WavesPortal.js
+++ src/components/WavesPortal/WavesPortal.js
@@ -1,12 +1,119 @@
 
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './WavesPortal.css';
 
+
+//contract
+import abi from './utils/WavePortal.json';
+
 export default function WavesPortal() {
 
-  const wave = () => {
+	const contractAddress = '0x3FF82f4E995Cf9F4aB8058dC50285edBdB932A06';
+	const contractABI = abi.abi;
 
+	/*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+
+	const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+	/**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+			// get rid of .then
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+	/*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+// we woud need to re-use this for the WAReport
+const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+
+
+        const signer = provider.getSigner();
+//        const signer = ethereum.selectedAddress;
+				debugger;
+//				if (!signer._address){ console.error('signer has no address'); return };
+
+
+
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+				debugger;
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -18,12 +125,24 @@ export default function WavesPortal() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Alejandro and I hack on the weekends so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
+			{currentAccount && (
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+			)}
+		    {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+
       </div>
     </div>
   );
