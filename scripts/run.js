287d9fc1db036c6801fa930cd6f17f9e10eff674
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-07 21:51:52 -0800

    waveCount added

--- scripts/run.js
+++ scripts/run.js
@@ -1,20 +1,19 @@
 const main = async () => {
-	const [owner, randomPerson] = await hre.ethers.getSigners();
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
 
-
   console.log("Contract deployed to:", waveContract.address);
 	console.log("Contract deployed by:", owner.address);
-
-
-  const waveTxn = await waveContract.wave();
-  await waveTxn.wait();
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
 
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
