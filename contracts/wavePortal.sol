54c27cde2b48c33defae22a42a225c86ef31e05a
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-08 22:50:51 -0800

    Updated wave contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -5,21 +5,39 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-		uint256 totalWaves;
+    uint256 totalWaves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    Wave[] waves;
 
     constructor() {
-        console.log("constructor: Yo zer0kool, I am a contract and I am smart");
+        console.log("I AM SMART CONTRACT. POG.");
     }
-		
-		function wave() public {
+
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-			if (totalWaves > 0){
-        	console.log("We have %d total waves!", totalWaves);
-				}
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
