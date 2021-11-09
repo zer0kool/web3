fd335717253693aa1a0a54cf4631cab0b87a3d3c
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-08 23:01:05 -0800

    run updated

--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,22 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-
-  console.log("Contract deployed to:", waveContract.address);
-	console.log("Contract deployed by:", owner.address);
+  console.log('Contract address:', waveContract.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  const waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  waveCount = await waveContract.getTotalWaves();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
@@ -25,5 +28,3 @@ const runMain = async () => {
     process.exit(1);
   }
 };
-
-runMain();
