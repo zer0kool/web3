ff83121918e2b55cfc4f60bc561beae7126156ef
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-09 20:25:18 -0800

    payable added to contract

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -17,7 +17,7 @@ contract WavePortal {
 
 	Wave[] waves;
 
-	constructor() {
+	constructor() payable {
 		console.log("I AM SMART CONTRACT. POG.");
 	}
 
--- scripts/deploys/WavePortal_deploy.js
+++ scripts/deploys/WavePortal_deploy.js
@@ -5,11 +5,15 @@ const main = async () => {
   console.log('Deploying contracts with account: ', deployer.address);
   console.log('Account balance: ', accountBalance.toString());
 
-  const Token = await hre.ethers.getContractFactory('WavePortal');
-  const portal = await Token.deploy();
-  await portal.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
 
-  console.log('WavePortal address: ', portal.address);
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.001'),
+  });
+
+  await waveContract.deployed();
+
+  console.log('WavePortal address: ', waveContract.address);
 };
 
 const runMain = async () => {
