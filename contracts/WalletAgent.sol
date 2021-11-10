39604a7908b241ee9140fe22ac4ba5f13c2d31e6
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-09 20:25:08 -0800

    walletAgent updated

--- contracts/WalletAgent.sol
+++ contracts/WalletAgent.sol
@@ -5,85 +5,75 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WalletReport {
-    uint256 totalReports;
+	uint256 totalReports;
 
-    event NewReport(
-			uint256 id,
-			uint256 timestamp,
-			string nickname,
-			address indexed from, 
-			string reportedAddress
-		);
-	
-	
-    //Event emitted when an address validates a report
-    event ValidateAddr(address indexed agent, uint256 id, uint256 timestamp);
+	event NewReport(
+		uint256 id,
+		uint256 timestamp,
+		//string nickname,
+		address indexed from,
+		string reportedAddress
+	);
 
 
-    struct Report {
-				uint256 id;
-			  uint256 timestamp; // The timestamp when the user reported the wallet.
-        address agent; // The address of the user who reported the wallet.
-				string nickname; // The nickname for the agent 
-        string reportedAddress; // The bad address the user sent.
-    }
+	//Event emitted when an address validates a report
+	event ValidateAddr(address indexed agent, uint256 id, uint256 timestamp);
 
-		// All the reports 
-    Report[] badWallets;
 
-	  // Mapping from address to track reports submitted by that address.
-    mapping(address => Report[]) reportsByAddress;
+	struct Report {
+		uint256 id;
+		uint256 timestamp; // The timestamp when the user reported the wallet.
+		address agent; // The address of the user who reported the wallet.
+		//string nickname; // The nickname for the agent 
+		string reportedAddress; // The bad address the user sent.
+	}
 
-    // Number of validations each report has.
-    mapping(uint256 => uint256) validations;
-
-    // Mapping to check whether a particular address has liked a particular track.
-    mapping(address => mapping(uint256 => bool)) validationsForAddress;
-	
-	
-	
-    constructor() {
-        console.log("Wallet Agent initialized");
-    }
-
-	
-	
-	
-    function report(string memory _reportedAddress, _nickname) public {
-				uint256 _id = badWallets.length;
-        
-				totalReports += 1;
-        console.log("%s has reported!", msg.sender);
-			
-			  Report memory _newReport = Report(
-						_id,
-					  block.timestamp,
-						_nickname,
-            msg.sender,
-            _reportedAddress
-        );
-
-        badWallets.push(_newReport);
-				reportsByAddress[msg.sender].push(_newReport);
-
-        emit NewReport(_id, block.timestamp, _nickname, msg.sender, _reportedAddress);
-    }
-
-	
-	
-    function getAllReports() public view returns (Report[] memory) {
-        return badWallets;
-    }
-	
+	// All the reports 
+	Report[] badWallets;
 	
 
-    function getTotalReports() public view returns (uint256) {
-        // Optional: Add this line if you want to see the contract print the value!
-        // We'll also print it over in run.js as well.
-        console.log("We have %d reported wallets!", totalReports);
-        return totalReports;
-    }
+	// Mapping from address to track reports submitted by that address.
+	mapping(address => Report[]) reportsByAddress;
+
+	// Number of validations each report has.
+	mapping(uint256 => uint256) validations;
+
+	// Mapping to check whether a particular address has validated a report
+	mapping(address => mapping(uint256 => bool)) validationsForAddress;
+
+	constructor() {console.log("Wallet Agent initialized");}
+
 	
-	
-	
+	function addReport(string memory _reportedAddress) public {
+		uint256 _id = badWallets.length;
+		
+		totalReports += 1;
+		console.log("%s has reported!", msg.sender);
+		
+		
+//		badWallets.push(Report(_id, _nickname, block.timestamp, msg.sender, _reportedAddress));
+		Report memory _newReport = Report(_id, block.timestamp, msg.sender, _reportedAddress);
+
+		badWallets.push(_newReport);
+		reportsByAddress[msg.sender].push(_newReport);
+//		reportsByAddress[msg.sender].push(Report(_id, _nickname, block.timestamp, msg.sender, _reportedAddress));
+
+		emit NewReport(_id, block.timestamp, msg.sender, _reportedAddress);
+	}
+
+
+
+	function getAllReports() public view returns(Report[] memory) {
+		return badWallets;
+	}
+
+
+
+	function getTotalReports() public view returns(uint256) {
+		// Optional: Add this line if you want to see the contract print the value!
+		// We'll also print it over in run.js as well.
+		console.log("We have %d reported wallets!", totalReports);
+		return totalReports;
+	}
+
 }
