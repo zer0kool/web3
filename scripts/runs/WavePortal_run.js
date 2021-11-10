d06819ba17e8ecd29e6075dccaed3a7526aa2701
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-09 16:59:12 -0800

    updating test run for wave contract

--- scripts/runs/WavePortal_run.js
+++ scripts/runs/WavePortal_run.js
@@ -1,33 +1,46 @@
 const main = async () => {
-  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-  const waveContract = await waveContractFactory.deploy({
-    value: hre.ethers.utils.parseEther('0.1'),
-  });
-  await waveContract.deployed();
-  console.log('Contract address:', waveContract.address);
+	const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
 
-  let waveCount;
-  waveCount = await waveContract.getTotalWaves();
-  console.log(waveCount.toNumber());
+	// The ammount of funding for the contract
+	const waveContract = await waveContractFactory.deploy({
+		value: hre.ethers.utils.parseEther('0.1'),
+	});
 
-  let waveTxn = await waveContract.wave('A message!');
-  await waveTxn.wait(); // Wait for the transaction to be mined
+	// Test and compile
+	await waveContract.deployed();
+	console.log('Contract address:', waveContract.address);
 
-  const [_, randomPerson] = await hre.ethers.getSigners();
-  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
-  await waveTxn.wait(); // Wait for the transaction to be mined
+	// Get Contract balance
+	let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+	console.log('Contract balance:', hre.ethers.utils.formatEther(contractBalance));
 
-  let allWaves = await waveContract.getAllWaves();
-  console.log(allWaves);
+	let waveCount;
+	waveCount = await waveContract.getTotalWaves();
+	console.log(waveCount.toNumber());
+
+	let waveTxn = await waveContract.wave('A message!');
+	await waveTxn.wait(); // Wait for the transaction to be mined
+
+	const [_, randomPerson] = await hre.ethers.getSigners();
+	waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+	await waveTxn.wait(); // Wait for the transaction to be mined
+
+	// Get Contract balance after two waves
+	let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+	console.log('Contract balance:', hre.ethers.utils.formatEther(contractBalance));
+
+	let allWaves = await waveContract.getAllWaves();
+	console.log(allWaves);
+
 };
 
 const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		await main();
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 runMain();
