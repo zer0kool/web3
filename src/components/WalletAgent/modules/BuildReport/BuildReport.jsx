2878aeb0282e4d18b768a86f6d46251ea7afc548
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-11 08:59:16 -0800

    init of modular component

---
+++ src/components/WalletAgent/modules/BuildReport/BuildReport.jsx
@@ -0,0 +1,5 @@
+import React, { Component, useEffect, useState  } from "react";
+import { ethers } from "ethers";
+import M from "materialize-css";
+import abi from '../utils/WalletAgent.json';
+import "./BuildReport.css";
