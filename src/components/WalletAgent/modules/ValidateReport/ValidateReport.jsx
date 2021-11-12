9c8000ebf7979c2b570e125447e81e426ab4255f
Alejandro Loaiza <zer0kool@users.noreply.github.com>
2021-11-11 16:50:17 -0800

    path updated

--- src/components/WalletAgent/modules/ValidateReport/ValidateReport.jsx
+++ src/components/WalletAgent/modules/ValidateReport/ValidateReport.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState  } from "react";
 import { ethers } from "ethers";
 import M from "materialize-css";
-import abi from '../utils/WalletAgent.json';
+import abi from './utils/WalletAgent.json';
 import "./ValidateReport.css";
